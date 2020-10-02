b4ac6f7ba2263d0d9c06a658ca296d37c7439471
Melisa Öztürk <mlsaoztrk@icloud.com>
2020-10-02 21:49:18 +0300

    static data

--- ETicaret/ViewModel.swift
+++ ETicaret/ViewModel.swift
@@ -22,12 +22,6 @@ class ViewModel: NSObject {
     override init() {
         super.init()
         self.ref = Database.database().reference()
-        
-        self.setProduct(id: 1, category: "Elektronik", title: "Bilgisayar", price: 10000.0, description: "abc")
-        self.setProduct(id: 2, category: "Beyaz Eşya", title: "Buz Dolabı", price: 10000.0, description: "abc")
-        self.setProduct(id: 3, category: "Elektronik", title: "Tv", price: 10000.0, description: "abc")
-        self.setProduct(id: 4, category: "Elektronik", title: "Bilgisayar", price: 10000.0, description: "abc")
-        self.setProduct(id: 5, category: "Elektronik", title: "Bilgisayar", price: 10000.0, description: "abc")
     }
     
 //    Veritabanını oluşturuyoruz.  Kullanıcının girdiği veriler de yine buradan ekleniyor
@@ -41,21 +35,44 @@ class ViewModel: NSObject {
         ref.child("products").observeSingleEvent(of: .value, with: { (snapshot) in
             let values = snapshot.value as? [String: AnyObject]
             
-            for (_, value) in values!.enumerated() {
-                let id = value.value["id"] as? Int
-                let title = value.value["title"] as? String
-                let category = value.value["category"] as? String
-                let price = value.value["price"] as? Double
-                let description = value.value["description"] as? String
+            if values == nil {
+                self.setProduct(id: 1, category: "Elektronik", title: "Bilgisayar", price: 10000.0, description: "abc")
+                self.setProduct(id: 2, category: "Beyaz Eşya", title: "Buz Dolabı", price: 10000.0, description: "abc")
+                self.setProduct(id: 3, category: "Elektronik", title: "Tv", price: 10000.0, description: "abc")
+                self.setProduct(id: 4, category: "Elektronik", title: "Bilgisayar", price: 10000.0, description: "abc")
+                self.setProduct(id: 5, category: "Elektronik", title: "Bilgisayar", price: 10000.0, description: "abc")
+                self.ref.child("products").observeSingleEvent(of: .value, with: { (snapshot) in
+                let values = snapshot.value as? [String: AnyObject]
+
+                  for (_, value) in values!.enumerated() {
+                      let id = value.value["id"] as? Int
+                      let title = value.value["title"] as? String
+                      let category = value.value["category"] as? String
+                      let price = value.value["price"] as? Double
+                      let description = value.value["description"] as? String
+                      
+                      self.products.append(Product(id: id, title: title, category: category, price: price, description: description))
+                      
+                  }
+               
+                  self.viewModelDelegate!.updateTableData(products: self.products)
+                }) { (error) in
+                    print(error.localizedDescription)
+                }
+            } else {
                 
-                self.products.append(Product(id: id, title: title, category: category, price: price, description: description))
-                
+                for (_, value) in values!.enumerated() {
+                    let id = value.value["id"] as? Int
+                    let title = value.value["title"] as? String
+                    let category = value.value["category"] as? String
+                    let price = value.value["price"] as? Double
+                    let description = value.value["description"] as? String
+                    
+                    self.products.append(Product(id: id, title: title, category: category, price: price, description: description))
+                    
+                }
+                self.viewModelDelegate!.updateTableData(products: self.products)
             }
-            
-            
-//            self.product = Product(id: id, title: title, category: category, price: price, description: description)
-            self.viewModelDelegate!.updateTableData(products: self.products)
-//
         }) { (error) in
             print(error.localizedDescription)
         }
